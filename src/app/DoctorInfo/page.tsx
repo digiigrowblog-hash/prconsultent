236d0b4cbbcf953c773ac3e903e3ac8ed2b967d9
digiigrowblog-hash <digiigrowblog@gmail.com>
2025-11-21 14:19:07 +0530

    add new name

--- src/app/DoctorInfo/page.tsx
+++ src/app/DoctorInfo/page.tsx
@@ -1,10 +1,9 @@
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 
-import DoctorinfoClient from './DoctorinfoClinet'
+import DoctorinfoClient from "./DoctorinfoClinic";
 
 // ...existing code...
 export default function Doctorinfo() {
   return <DoctorinfoClient />;
 }
-// ...existing code...
