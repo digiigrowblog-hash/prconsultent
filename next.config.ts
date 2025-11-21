0147a144b209b0de5da8ec770f8eb3a23e44eebd
digiigrowblog-hash <digiigrowblog@gmail.com>
2025-11-21 11:42:14 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   // Add this if you're using static exports
-  output: 'standalone',
   // If you're using API routes, add this:
   async rewrites() {
     return [
